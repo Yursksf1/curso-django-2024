14b640eb38f11693541c923a5f98753714643f99
YursKsf1 <yurs.ksf@gmail.com>
2024-07-18 19:27:37 -0500

    update config server and add app

--- proyectos_django/mysite/settings.py
+++ proyectos_django/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mm^5tz4!$3%&0r&8fsi4&zdl51^rl6)=^n1_-m5tv9rwdn#$&t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp.apps.MyappConfig'
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
