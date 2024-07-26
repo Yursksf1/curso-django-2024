162a030c23931da64f12c48600efde3c5a433061
YursKsf1 <yurs.ksf@gmail.com>
2024-07-25 20:27:48 -0500

    primer hola mundo desde el navegador

--- proyectos_django/myapp/views.py
+++ proyectos_django/myapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def hola_mundo(request):
+    print('hola mundo estoy desde el servidor de django')
+
+    html = "<html><body>hola mundo desde el navegador, mi nombre es: Yurley </body></html>"
+    return HttpResponse(html)
